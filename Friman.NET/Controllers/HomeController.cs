136950fee0a51a2ee39377680f5e93a5440b8972
Denis <45011033+feezeee@users.noreply.github.com>
2021-10-22 10:56:01 +0300

    Добавьте файлы проекта.

---
+++ Friman.NET/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Friman.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friman.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();            
+        }
+
+        
+
+
+    }
+}
---
+++ Friman.NET/Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friman.NET.Models
+{
+    public class GuestResponse
+    {
+        public string Name;
+        public string Email { get; set; }        
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
